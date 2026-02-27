f22bc4ae67907dffcdbf13bed348b9ab4d190210
celiadelacruz89-gif <celiadelacruz89@gmail.com>
2026-02-27 13:53:42 -0700

    Add metadata and layout for RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Stateline Renegades",
+  description: "Stateline Renegades Cheer & T-Ball Organization"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
