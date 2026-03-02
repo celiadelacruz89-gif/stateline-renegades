3a370dd336edc01b018a3dd88cc7eeb3fc8b52fd
celiadelacruz89-gif <celiadelacruz89@gmail.com>
2026-03-02 10:51:57 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images:{ unoptimized:true } };
-module.exports = nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
